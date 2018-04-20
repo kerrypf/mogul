4fe40b00245d8ae2c42b781b31ccab522afd15e4
roger <guyuanhao@suiyi.com.cn>
2018-04-20 11:25:54 +0800

    temp

--- plopfile.js
+++ plopfile.js
@@ -1,16 +1,10 @@
 module.exports = plop => {
-  plop.setHelper(
-    'pascal',
-    text =>
-      `${text.substr(0, 1).toUpperCase()}${text.substring(1, text.length)}`
-  );
   const templates = [
     'Collection',
     'Detail',
     'Editor',
     'index',
     'Query',
-    'Store',
     'services/api',
     'services/collection',
     'services/pagination',
@@ -26,22 +20,22 @@ module.exports = plop => {
       }
     ],
     actions: [
-      ...templates.map(name => ({
+      ...templates.map(file => ({
         type: 'add',
-        templateFile: `./node_modules/mogul/plop-templates/${name}.js.hbs`,
-        path: `./src/pages/{{pascal name}}/${name}.js`
+        templateFile: `./node_modules/mogul/plop-templates/${file}.js.hbs`,
+        path: `./src/pages/{{pascalCase name}}/${file}.js`
       })),
       {
         type: 'append',
         pattern: '{/*insert:route*/}',
         template:
-          '<Route path={`${url}/{{name}}`} component={ {{pascal name}} } />',
+          '<Route path={`${url}/{{dashCase name}}`} component={ {{pascalCase name}} } />',
         path: `./src/pages/Dashboard/index.js`
       },
       {
         type: 'append',
         pattern: 'insert:component',
-        template: "import {{pascal name}} from '../{{pascal name}}';",
+        template: "import {{pascalCase name}} from '../{{pascalCase name}}';",
         path: `./src/pages/Dashboard/index.js`
       }
     ]
