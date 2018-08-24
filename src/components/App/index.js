2cccd3e482394f258d4fcfeacedbe283d196c35f
zhangqing <zhangqing332@live.com>
2018-08-24 09:02:11 +0800

    Publish Version 0.0.61-alpha.5019

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -107,9 +107,10 @@ export default class App extends Component {
               <Sider routes={renderRoutes.filter(route => route.type !== "redirect")} />
 
               <AppContainer direction={"column"} flex={1}>
-                <Observer>
-                  {() => (header && !configuration.fullScreen ? <Header>{header}</Header> : null)}
-                </Observer>
+                <Header>{header}</Header>
+                {/*<Observer>*/}
+                  {/*{() => (header && !configuration.fullScreen ? <Header>{header}</Header> : null)}*/}
+                {/*</Observer>*/}
                 <Content>
                   <Switch>{this.renderRoutes(renderRoutes)}</Switch>
                 </Content>
